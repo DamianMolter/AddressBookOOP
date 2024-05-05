0daccd2fd7c72b737c7b9807978795a4703cf660
Damian Molter <damian.molter.programista@gmail.com>
2024-05-05 22:34:03 +0200

    Added saving users to a text file

--- KsiazkaAdresowa.h
+++ KsiazkaAdresowa.h
@@ -2,6 +2,8 @@
 #include <vector>
 #include <windows.h>
 #include "Uzytkownik.h"
+#include <fstream>
+#include <sstream>
 
 using namespace std;
 
@@ -9,13 +11,21 @@ class KsiazkaAdresowa {
     int idZalogowanegoUzytkownika;
     int idOstatniegoAdresata;
     int idUsunietegoAdresata;
+    int liczba;
     vector <Uzytkownik> uzytkownicy;
+    string nazwaPlikuZUzytkownikami;
+    fstream plikTekstowy;
 
     Uzytkownik podajDaneNowegoUzytkownika();
     int pobierzIdNowegoUzytkownika();
     bool czyIstniejeLogin(string login);
+    void dopiszUzytkownikaDoPliku(Uzytkownik uzytkownik);
+    string zamienDaneUzytkownikaNaLinieZDanymiOddzielonaPionowymiKreskami(Uzytkownik uzytkownik);
+    bool czyPlikJestPusty(fstream &plikTekstowy);
+    string konwerjsaIntNaString(int liczba);
 
 public:
+    KsiazkaAdresowa();
     void rejestracjaUzytkownika();
     void wyswietlWszystkichUzytkownikow();
 };
