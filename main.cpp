d947e906a555b5b35c984636ce2e7f434203b0c2
Damian Molter <damian.molter.programista@gmail.com>
2026-05-19 11:52:39 +0200

    From now main only chooses filenames and run the app

--- main.cpp
+++ main.cpp
@@ -1,69 +1,13 @@
 #include <iostream>
-#include "AddressBook.h"
+#include "App.h"
 #include "windows.h"
 
 using namespace std;
 
 int main() {
 
-    AddressBook addressBook("Users.txt" , "Recipients.txt");
-
-    while (true) {
-        if (!addressBook.isUserLogged()) {
-            char choice = addressBook.pickOptionMainMenu();
-
-            switch (choice) {
-            case '1':
-                addressBook.userRegister();
-                break;
-            case '2':
-                addressBook.userSignIn();
-                break;
-            case '9':
-                exit(0);
-                break;
-            default:
-                cout << endl << "Nie ma takiej opcji w menu." << endl << endl;
-                system("pause");
-                break;
-            }
-        } else {
-            char choice = addressBook.pickOptionUserMenu();
-
-            switch (choice) {
-            case '1':
-                addressBook.addRecipient();
-                break;
-            case '2':
-                addressBook.findRecipientByName();
-                break;
-            case '3':
-                addressBook.findRecipientBySurname();
-                break;
-            case '4':
-                addressBook.displayAllRecipients();
-                break;
-            case '5':
-                addressBook.deleteRecipient();
-                break;
-            case '6':
-                addressBook.editRecipient();
-                break;
-            case '7':
-                addressBook.loggedUserPasswordChange();
-                break;
-            case '8':
-                addressBook.userLogout();
-                break;
-            }
-        }
-    }
-
-
-
-
-
-
+    App app("Users.txt" , "Recipients.txt");
+    app.run();
 
     /*AddressBook addressBook;
     addressBook.wyswietlWszystkichUzytkownikow();
@@ -72,5 +16,5 @@ int main() {
     addressBook.wyswietlWszystkichAdresatow();
     addressBook.wylogowanieUzytkownika();*/
 
-    return 0;
+
 }
